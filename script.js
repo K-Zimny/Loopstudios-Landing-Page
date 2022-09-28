aafe39f92e5f0f708386a05ee89d4b8b4570c657
kzim <k.zimny1994@gmail.com>
2022-09-28 17:02:46 -0500

    Continuded project, cleaned up footer

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+// Navigation - mobile pop up
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 const navUl = document.querySelector(".nav ul");
 
 navToggle.addEventListener("click", function () {
-  if (nav.classList.contains("hidden")) {
-    nav.classList.remove("hidden");
-    navUl.classList.remove("hidden");
+  if (nav.classList.contains("shown")) {
+    nav.classList.remove("shown");
+    navUl.classList.remove("shown");
   } else {
-    nav.classList.add("hidden");
-    navUl.classList.add("hidden");
+    nav.classList.add("shown");
+    navUl.classList.add("shown");
   }
 });
+
+//button div movement
+
+const buttonContentGrid4x2 = document.getElementById("content-grid-4x2-button");
+const buttonLocationContentGrid4x2Before = document.getElementById(
+  "content-grid-4x2-button-relocation-before"
+);
+const buttonLocationContentGrid4x2After = document.getElementById(
+  "content-grid-4x2-button-relocation-after"
+);
+
+if (window.matchMedia("(max-width: 700px)").matches) {
+  console.log("test");
+  buttonLocationContentGrid4x2Before.appendChild(buttonContentGrid4x2);
+} else {
+}
